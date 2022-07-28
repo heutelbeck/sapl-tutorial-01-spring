d41d643a97d74abafedf2300d95fa3522fb8e538
Dominic Heutelbeck <heutelbeck@users.noreply.github.com>
2022-07-28 18:28:20 +0200

    basic project scaffolding

---
+++ src/main/java/io/sapl/tutorial/SaplTutorial01SpringApplication.java
@@ -0,0 +1,13 @@
+package io.sapl.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaplTutorial01SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaplTutorial01SpringApplication.class, args);
+	}
+
+}
