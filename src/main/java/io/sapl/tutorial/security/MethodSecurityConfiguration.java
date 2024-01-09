1b2ae1108f8e2b18bad7637d78b42b4a9dec28cc
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-01-09 14:41:58 +0100

    migrate to 3.0.0

--- src/main/java/io/sapl/tutorial/security/MethodSecurityConfiguration.java
+++ src/main/java/io/sapl/tutorial/security/MethodSecurityConfiguration.java
@@ -1,25 +1,21 @@
 package io.sapl.tutorial.security;
 
-import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.sapl.api.pdp.PolicyDecisionPoint;
-import io.sapl.spring.config.SaplMethodSecurityConfiguration;
-import io.sapl.spring.constraints.ConstraintEnforcementService;
-import io.sapl.spring.subscriptions.AuthorizationSubscriptionBuilderService;
+import io.sapl.spring.config.EnableSaplMethodSecurity;
 
 @Configuration
-@EnableGlobalMethodSecurity
-public class MethodSecurityConfiguration extends SaplMethodSecurityConfiguration {
+@EnableWebSecurity
+@EnableSaplMethodSecurity
+public class MethodSecurityConfiguration  {
 
-	public MethodSecurityConfiguration(ObjectFactory<PolicyDecisionPoint> pdpFactory,
-			ObjectFactory<ConstraintEnforcementService> constraintHandlerFactory,
-			ObjectFactory<ObjectMapper> objectMapperFactory,
-			ObjectFactory<AuthorizationSubscriptionBuilderService> subscriptionBuilderFactory) {
-		super(pdpFactory, constraintHandlerFactory, objectMapperFactory, subscriptionBuilderFactory);
-	}
-
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
 }
