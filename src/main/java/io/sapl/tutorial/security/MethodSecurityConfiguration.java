1f656333ffa53aac07bd7ca70f3668ea320936c5
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-01-09 15:19:14 +0100

    Update MethodSecurityConfiguration.java

--- src/main/java/io/sapl/tutorial/security/MethodSecurityConfiguration.java
+++ src/main/java/io/sapl/tutorial/security/MethodSecurityConfiguration.java
@@ -1,21 +1,13 @@
 package io.sapl.tutorial.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import io.sapl.spring.config.EnableSaplMethodSecurity;
 
 @Configuration
 @EnableWebSecurity
 @EnableSaplMethodSecurity
-public class MethodSecurityConfiguration  {
+public class MethodSecurityConfiguration {
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    
 }
