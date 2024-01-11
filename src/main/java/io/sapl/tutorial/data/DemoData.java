f8d34da74a35655b6c94124b7e1562aa5174546b
Dominic Heutelbeck <dheutelbeck@ftk.de>
2024-01-11 09:50:49 +0100

    add bcrypt

--- src/main/java/io/sapl/tutorial/data/DemoData.java
+++ src/main/java/io/sapl/tutorial/data/DemoData.java
@@ -1,8 +1,13 @@
 package io.sapl.tutorial.data;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import io.sapl.tutorial.domain.Book;
@@ -15,24 +20,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DemoData implements CommandLineRunner {
 
-	private final BookRepository bookRepository;
-	private final LibraryUserDetailsService userDetailsService;
+    public static final String DEFAULT_RAW_PASSWORD = "password";
 
-	@Override
-	public void run(String... args) {
-		// @formatter:off
+    private final BookRepository            bookRepository;
+    private final LibraryUserDetailsService userDetailsService;
+    private final PasswordEncoder           encoder;
+
+    @Override
+    public void run(String... args) {
+        // @formatter:off
 		bookRepository.save(new Book(1L, "Clifford: It's Pool Time!",                                  0, "*Woof*"));
 		bookRepository.save(new Book(2L, "The Rescue Mission: (Pokemon: Kalos Reader #1)",             4, "Gotta catch 'em all!"));
 		bookRepository.save(new Book(3L, "Dragonlance Chronicles Vol. 1: Dragons of Autumn Twilight",  9, "Some fantasy story."));
 		bookRepository.save(new Book(4L, "The Three-Body Problem",                                    14, "Space is scary."));
 
-		userDetailsService.load(new LibraryUser("zoe",   birthdayForAgeInYears(17), "{noop}password"));
-		userDetailsService.load(new LibraryUser("bob",   birthdayForAgeInYears(10), "{noop}password"));
-		userDetailsService.load(new LibraryUser("alice", birthdayForAgeInYears(3),  "{noop}password"));
+		userDetailsService.load(new LibraryUser("zoe",   birthdayForAgeInYears(17), encoder.encode(DEFAULT_RAW_PASSWORD)));
+		userDetailsService.load(new LibraryUser("bob",   birthdayForAgeInYears(10), encoder.encode(DEFAULT_RAW_PASSWORD)));
+		userDetailsService.load(new LibraryUser("alice", birthdayForAgeInYears(3),  encoder.encode(DEFAULT_RAW_PASSWORD)));
 		// @formatter:on
-	}
+    }
 
-	private LocalDate birthdayForAgeInYears(int age) {
-		return LocalDate.now().minusYears(age).minusDays(20);
-	}
+    private LocalDate birthdayForAgeInYears(int age) {
+        return LocalDate.now().minusYears(age).minusDays(20);
+    }
+
 }
--- src/main/java/io/sapl/tutorial/security/SecurityConfiguration.java
+++ src/main/java/io/sapl/tutorial/security/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import io.sapl.spring.config.EnableSaplMethodSecurity;
@@ -21,4 +23,10 @@ public class SecurityConfiguration {
                    .build();
         // @formatter:on
     }
+    
+    @Bean
+    static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
